8aec2ec5c1e6a1cbb43d5d23676c19daa8b47329
ryumarfin <ryumarfin.t@gmail.com>
2021-01-30 16:24:14 +0700

    repair profile fragment

--- app/src/main/java/com/example/latber/fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/latber/fragments/ProfileFragment.kt
@@ -1,14 +1,17 @@
 package com.example.latber.fragments
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.GridView
 import com.example.latber.MainActivity
 import com.example.latber.R
+import com.example.latber.Sparepart_Items_Adapter
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -38,9 +41,13 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
-
-
+        var objView = inflater.inflate(R.layout.fragment_profile, container, false)
+        var btn = objView.findViewById<Button>(R.id.btn_logout)
+        btn.setOnClickListener {
+            var intentBaru = Intent(activity, MainActivity::class.java)
+            startActivity(intentBaru)
+        }
+        return objView
 
         }
 
