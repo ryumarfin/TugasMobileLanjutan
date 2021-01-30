bd2e51d46881723f3671611db4402c269ebc67f2
ryumarfin <ryumarfin.t@gmail.com>
2021-01-30 18:29:23 +0700

    panggil montir done

---
+++ app/src/main/java/com/example/latber/dapatMontir.kt
@@ -0,0 +1,20 @@
+package com.example.latber
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class dapatMontir : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dapat_montir)
+    }
+
+    fun toRating(view: View) {
+        var intentSparePart = Intent(this,rating::class.java)
+        startActivity(intentSparePart)
+        finish()
+    }
+
+}
