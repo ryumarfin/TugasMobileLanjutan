eff740e0bb4872467acb562ce7ba9dab29875183
Haqim28 <amirul2810002@gmail.com>
2021-03-11 19:03:24 +0700

    19:02 Maret 11 2021

--- app/src/main/java/com/example/latber/Register.kt
+++ app/src/main/java/com/example/latber/Register.kt
@@ -4,6 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_register.*
+
+//2. pembuatan extra status
+private const val EXTRA_STATUS = "ExtraStatus"
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +19,22 @@ class Register : AppCompatActivity() {
         var intentSparePart = Intent(this,menu::class.java)
         startActivity(intentSparePart)
         finish()
+    }
+
+    fun cek(view: View) {
+
+        statusMail.setText( " " + et_Email_Register.text + "   Belum terdaftar")
+    }
+
+    //1. Pembuatan save instance state
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(EXTRA_STATUS, statusMail.text.toString())
+    }
+
+    //3. pembacaan saveinstance state
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        statusMail.text =   savedInstanceState?.getString(EXTRA_STATUS) ?: "-"
     }
 }
