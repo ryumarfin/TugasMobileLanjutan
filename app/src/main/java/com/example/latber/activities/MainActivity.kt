ade53624453e61fd5a6c9d08ee54324ac178af82
Gravelazer <gardawilson@gmail.com>
2021-03-18 22:05:22 +0700

    Pembuatan air plane receiver

--- app/src/main/java/com/example/latber/activities/MainActivity.kt
+++ app/src/main/java/com/example/latber/activities/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.latber.activities
 
 import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.latber.R
 import com.example.latber.Register
+import com.example.latber.airPlaneReceiver
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +15,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Melakukan register receiver di mainactivity
+        var AirplaneReceiver = airPlaneReceiver()
+        var filter = IntentFilter()
+        filter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        registerReceiver(AirplaneReceiver, filter)
     }
+
+
     fun toRegisterPage(view: View) {
         var intentRegister = Intent(this, Register::class.java)
         startActivity(intentRegister)
