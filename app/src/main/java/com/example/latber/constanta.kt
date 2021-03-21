a3540330efc2b7e889928eecebd9be9c16982726
ryumarfin <ryumarfin.t@gmail.com>
2021-03-21 19:13:55 +0700

    M03 - Penggunaan Receiver pengecekan status perangkat

--- app/src/main/java/com/example/latber/constanta.kt
+++ app/src/main/java/com/example/latber/constanta.kt
@@ -5,3 +5,9 @@ const val EXTRA_HARGA = "com.example.latber.HARGA"
 
 // membuat key
 const val DETAIL_ITEM = "detail_item"
+
+const val REQUEST_CAMERA = 123
+
+const val REQUEST_TAKEPICTURE = 124
+
+const val RESULT_CAMERA = 125
