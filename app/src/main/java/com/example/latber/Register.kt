ccb290c6ceb2e33d69d29b0572f2ae51f9c71dca
Haqim28 <amirul2810002@gmail.com>
2021-04-09 17:51:10 +0700

    notify

--- app/src/main/java/com/example/latber/Register.kt
+++ app/src/main/java/com/example/latber/Register.kt
@@ -1,16 +1,23 @@
 package com.example.latber
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.media.MediaPlayer
+import android.os.*
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.Message
+import android.provider.MediaStore
 import android.view.View
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.bumptech.glide.Glide
 import com.example.latber.activities.menu
 import com.example.latber.fragments.imageView
@@ -27,9 +34,18 @@ import java.net.URL
 private const val EXTRA_STATUS = "ExtraStatus"
 
 class Register : AppCompatActivity() {
+    //Membuat channel khusus untuk notifikasi ini
+    private val CHANNEL_ID = "channel_id_example_01"
+    private val notificationId = 101
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        createNotificationChannel()
 
 
 
@@ -77,6 +93,61 @@ class Register : AppCompatActivity() {
 
     }
 
+    //Membuat notifikasi channel agar notifikasi dapat muncul
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name = "Notification Title"
+            val descriptionText = "Notification Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel: NotificationChannel = NotificationChannel(CHANNEL_ID,name,importance).apply {
+                description = descriptionText
+            }
+            //Deklarasi notifikasi manager
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    //fungsi untuk memunculkan atau mengirim notifikasi
+    private fun sendNotification(){
+        //buat intent agar ketika notifikasi di klik maka beralih ke menu activity
+        val intent:Intent = Intent(this,menu::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        //membuat pending intent
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this
+                ,0,intent,0)
+
+        //menkonversi gambar menjadi bitmap agar dapat dimunculkan ke notifikasi
+        val bitmap = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.montirface)
+        val bitmapLargeIcon = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.logo)
+
+
+
+        //mengambil nama pendaftar agar dapat muncul di notifikasi
+        val nama = findViewById<EditText>(R.id.et_firstname_Register).text
+
+        //membangun sebuah notifikasi dan mengatur isi didalamnya
+        // seperti judul, isi notifikasi dan lainnya.
+        val builder = NotificationCompat.Builder(this,CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                //menampilkan judul notifikasi
+                .setContentTitle("MarketPlace Registration")
+                //menampilkan detail text notifikasi
+                .setContentText("Terima kasih $nama telah mendaftar ")
+                //menambahkan icon
+                .setLargeIcon(bitmapLargeIcon)
+                //menambahkan gambar ke dalam notifikasi
+                .setStyle(NotificationCompat.BigPictureStyle().bigPicture(bitmap))
+              //  memanggil pending intent agar dapat melakukan aksi ketika di klik
+                .setContentIntent(pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)){
+            notify(notificationId,builder.build())
+        }
+    }
     private fun processBitMap(url : String): Bitmap? {
         return try{
             val url = URL(url)
@@ -95,6 +166,10 @@ class Register : AppCompatActivity() {
     }
 
     fun RegistertoSparepartPage1(view: View) {
+        //Panggil fungsi untuk memunculkan notifikasi di salah satu button yang
+        //telah anda tentukan
+        sendNotification()
+
         var intentSparePart = Intent(this, menu::class.java)
         startActivity(intentSparePart)
         finish()
