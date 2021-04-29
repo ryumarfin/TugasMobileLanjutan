a42ee84b359b9f66d5038c4a56658672b54c928b
ryumarfin <ryumarfin.t@gmail.com>
2021-04-29 22:49:54 +0700

    edit Register.kt and adding RegisterTest.kt

--- app/src/main/java/com/example/latber/Register.kt
+++ app/src/main/java/com/example/latber/Register.kt
@@ -12,11 +12,13 @@ import android.media.MediaPlayer
 import android.os.*
 import androidx.appcompat.app.AppCompatActivity
 import android.provider.MediaStore
+import android.util.Patterns
 import android.view.Menu
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.app.TaskStackBuilder
@@ -43,20 +45,17 @@ class Register : AppCompatActivity() {
     private val notificationId = 101
 
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        createNotificationChannel()
+//        createNotificationChannel()
 
 
+//        val imgView = findViewById<ImageView>(R.id.imgV)
+//        val tambahFoto = findViewById<TextView>(R.id.tambahFoto)
 
-        val imgView = findViewById<ImageView>(R.id.imgV)
-        val tambahFoto = findViewById<TextView>(R.id.tambahFoto)
-
-        var strURL = "http://blogs.quovantis.com/wp-content/uploads/2017/05/Kotlin-A_Post.png"
+        /*var strURL = "http://blogs.quovantis.com/wp-content/uploads/2017/05/Kotlin-A_Post.png"
 
         val handler= object : Handler(Looper.getMainLooper()){
             override fun handleMessage(inputMessage: Message) {
@@ -64,127 +63,181 @@ class Register : AppCompatActivity() {
                 imgView.setImageBitmap(photoTask)
             }
 
+        }*/
+//        tambahFoto.setOnClickListener{
+//            Thread(Runnable {
+//                doAsync {
+//                    var myPhoto = processBitMap(strURL)
+//                    uiThread {
+//                        imgView.setImageBitmap(myPhoto)
+//                    }
+//
+//
+//                }
+//                //val bitmap = processBitMap(strURL)
+//
+//                /*val msg = Message.obtain(handler)
+//                msg.obj = bitmap
+//                msg.sendToTarget()*/
+//
+//               /*imgView.post{
+//                    println("menambahkan foto profil")
+//                    imgView.setImageBitmap(bitmap)
+//                }*/
+//            }).start()
+//        }
+
+        fun Toasttt(text:String) {
+            Toast.makeText(this@Register, text, Toast.LENGTH_SHORT).show()
         }
-        tambahFoto.setOnClickListener{
-            Thread(Runnable {
-                doAsync {
-                    var myPhoto = processBitMap(strURL)
-                    uiThread {
-                        imgView.setImageBitmap(myPhoto)
-                    }
 
+        btn_Register.setOnClickListener(){
+            val firtName = et_firstname_Register.text.toString()
+            if(firtName.trim() == ""){
+                Toasttt("Please fill your First Name")
+                return@setOnClickListener
+            }
 
+            val lastName = et_lastname_Register.text.toString()
+            if(lastName.trim() == ""){
+                Toasttt("Please fill your Last Name")
+                return@setOnClickListener
+            }
+
+            val email = et_Email_Register.text.toString()
+            if(email.trim() == ""){
+                Toasttt("Please fill your email")
+                return@setOnClickListener
+            }
+            else{
+                if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    Toasttt("Email is Invalid!")
+                    return@setOnClickListener
                 }
+            }
+
+            val password = et_Password_Register.text.toString()
+            if(password.trim() == ""){
+                Toasttt("Please fill your Password")
+                return@setOnClickListener
+            }
+
+            val confirmPassword = et_confirmPassword_Register.text.toString()
+            if(confirmPassword.trim() == ""){
+                Toasttt("Please fill your Confirm Password")
+                return@setOnClickListener
+            }
+            else {
+                if(confirmPassword != password) {
+                    Toasttt("Confirm Password is wrong")
+                    return@setOnClickListener
+                }
+            }
 
 
-                //val bitmap = processBitMap(strURL)
+            //Panggil fungsi untuk memunculkan notifikasi di salah satu button yang
+            //telah anda tentukan
+//            sendNotification()
 
-                /*val msg = Message.obtain(handler)
-                msg.obj = bitmap
-                msg.sendToTarget()*/
-
-               /*imgView.post{
-                    println("menambahkan foto profil")
-                    imgView.setImageBitmap(bitmap)
-                }*/
-
-
-
-            }).start()
-
-
+            var intentSparePart = Intent(this, menu::class.java)
+            startActivity(intentSparePart)
+            finish()
         }
+
+    }
 
     }
 
     //Membuat notifikasi channel agar notifikasi dapat muncul
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            val name = "Notification Title"
-            val descriptionText = "Notification Description"
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel: NotificationChannel = NotificationChannel(CHANNEL_ID,name,importance).apply {
-                description = descriptionText
-            }
-            //Deklarasi notifikasi manager
-            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//    private fun createNotificationChannel() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+//            val name = "Notification Title"
+//            val descriptionText = "Notification Description"
+//            val importance = NotificationManager.IMPORTANCE_DEFAULT
+//            val channel: NotificationChannel = NotificationChannel(CHANNEL_ID,name,importance).apply {
+//                description = descriptionText
+//            }
+//            //Deklarasi notifikasi manager
+//            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//
+//            notificationManager.createNotificationChannel(channel)
+//        }
+//    }
+//
+//    //fungsi untuk memunculkan atau mengirim notifikasi
+//    private fun sendNotification(){
+//        //buat intent agar ketika notifikasi di klik maka beralih ke ProfileFragment
+//        val intent:Intent = Intent(this,menu::class.java).apply {
+//            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+//        }
+//
+//        //membaut TaskStackBuilder untuk memulai pendingIntent dan sekaligus backstack nya
+//        var myPendingIntent = TaskStackBuilder.create(this)
+//                .addNextIntentWithParentStack(intent)       //this (register.kt) akan menjadi Parent dari NotifyDetailIntent
+//                .getPendingIntent(11111, PendingIntent.FLAG_UPDATE_CURRENT)
+//
+//
+//        //membuat pending intent
+////        val pendingIntent: PendingIntent = PendingIntent.getActivity(this
+////                ,0,intent,0)
+//
+//        //menkonversi gambar menjadi bitmap agar dapat dimunculkan ke notifikasi
+//        val bitmap = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.montirface)
+//        val bitmapLargeIcon = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.logo)
+//
+//
+//
+//        //mengambil nama pendaftar agar dapat muncul di notifikasi
+//        val nama = findViewById<EditText>(R.id.et_firstname_Register).text
+//
+//        //membangun sebuah notifikasi dan mengatur isi didalamnya
+//        // seperti judul, isi notifikasi dan lainnya.
+//        val builder = NotificationCompat.Builder(this,CHANNEL_ID)
+//                .setSmallIcon(R.drawable.ic_launcher_foreground)
+//                //menampilkan judul notifikasi
+//                .setContentTitle("MarketPlace Registration")
+//                //menampilkan detail text notifikasi
+//                .setContentText("Terima kasih $nama telah mendaftar ")
+//                //menambahkan icon
+//                .setLargeIcon(bitmapLargeIcon)
+//                //menambahkan gambar ke dalam notifikasi
+//                .setStyle(NotificationCompat.BigPictureStyle().bigPicture(bitmap))
+//              //  memanggil pending intent agar dapat melakukan aksi ketika di klik
+//                .setContentIntent(myPendingIntent)
+//                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+//
+//        with(NotificationManagerCompat.from(this)){
+//            notify(notificationId,builder.build())
+//        }
+//    }
+//    private fun processBitMap(url : String): Bitmap? {
+//        return try{
+//            val url = URL(url)
+//            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+//            connection.doInput = true
+//            connection.connect()
+//            val input: InputStream = connection.inputStream
+//            val myBitmap = BitmapFactory.decodeStream(input)
+//
+//            myBitmap
+//        }catch (e: IOException){
+//            e.printStackTrace()
+//            null
+//        }
+//
+//    }
 
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
-
-    //fungsi untuk memunculkan atau mengirim notifikasi
-    private fun sendNotification(){
-        //buat intent agar ketika notifikasi di klik maka beralih ke ProfileFragment
-        val intent:Intent = Intent(this,menu::class.java).apply {
-            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        }
-
-        //membaut TaskStackBuilder untuk memulai pendingIntent dan sekaligus backstack nya
-        var myPendingIntent = TaskStackBuilder.create(this)
-                .addNextIntentWithParentStack(intent)       //this (register.kt) akan menjadi Parent dari NotifyDetailIntent
-                .getPendingIntent(11111, PendingIntent.FLAG_UPDATE_CURRENT)
 
 
-        //membuat pending intent
-//        val pendingIntent: PendingIntent = PendingIntent.getActivity(this
-//                ,0,intent,0)
-
-        //menkonversi gambar menjadi bitmap agar dapat dimunculkan ke notifikasi
-        val bitmap = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.montirface)
-        val bitmapLargeIcon = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.logo)
-
-
-
-        //mengambil nama pendaftar agar dapat muncul di notifikasi
-        val nama = findViewById<EditText>(R.id.et_firstname_Register).text
-
-        //membangun sebuah notifikasi dan mengatur isi didalamnya
-        // seperti judul, isi notifikasi dan lainnya.
-        val builder = NotificationCompat.Builder(this,CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                //menampilkan judul notifikasi
-                .setContentTitle("MarketPlace Registration")
-                //menampilkan detail text notifikasi
-                .setContentText("Terima kasih $nama telah mendaftar ")
-                //menambahkan icon
-                .setLargeIcon(bitmapLargeIcon)
-                //menambahkan gambar ke dalam notifikasi
-                .setStyle(NotificationCompat.BigPictureStyle().bigPicture(bitmap))
-              //  memanggil pending intent agar dapat melakukan aksi ketika di klik
-                .setContentIntent(myPendingIntent)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-
-        with(NotificationManagerCompat.from(this)){
-            notify(notificationId,builder.build())
-        }
-    }
-    private fun processBitMap(url : String): Bitmap? {
-        return try{
-            val url = URL(url)
-            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
-            connection.doInput = true
-            connection.connect()
-            val input: InputStream = connection.inputStream
-            val myBitmap = BitmapFactory.decodeStream(input)
-
-            myBitmap
-        }catch (e: IOException){
-            e.printStackTrace()
-            null
-        }
-
-    }
-
-    fun RegistertoSparepartPage1(view: View) {
-        //Panggil fungsi untuk memunculkan notifikasi di salah satu button yang
-        //telah anda tentukan
-        sendNotification()
-
-        var intentSparePart = Intent(this, menu::class.java)
-        startActivity(intentSparePart)
-        finish()
-    }
+//    fun RegistertoSparepartPage1(view: View) {
+//        //Panggil fungsi untuk memunculkan notifikasi di salah satu button yang
+//        //telah anda tentukan
+//        sendNotification()
+//
+//        var intentSparePart = Intent(this, menu::class.java)
+//        startActivity(intentSparePart)
+//        finish()
+//    }
 
  /*   fun cek(view: View) {
 
@@ -202,4 +255,4 @@ class Register : AppCompatActivity() {
         super.onRestoreInstanceState(savedInstanceState)
         statusMail.text =   savedInstanceState?.getString(EXTRA_STATUS) ?: "-"
     }*/
-}
+//}
