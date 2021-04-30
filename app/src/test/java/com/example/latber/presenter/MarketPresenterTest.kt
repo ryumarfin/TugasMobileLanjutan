8720d5eef1ff1698371c6b83374e2877bc3d57f1
ryumarfin <ryumarfin.t@gmail.com>
2021-04-30 23:05:35 +0700

    camera galery test OK

---
+++ app/src/test/java/com/example/latber/presenter/MarketPresenterTest.kt
@@ -0,0 +1,22 @@
+package com.example.latber.presenter
+
+import com.example.latber.Interface.MarketInterface
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.mockito.Mockito.mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class MarketPresenterTest {
+
+    //duplikat operasi dlm MarketInterface
+    private var view : MarketInterface = mock(MarketInterface::class.java)
+    //kirim view ke presenter
+    private var presenter = MarketPresenter(view)
+    @Test
+    fun itemList() {
+//        var
+    }
+}
