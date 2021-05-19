2172fdf16cc858dbe94dde35b844189ef112679f
ryumarfin <ryumarfin.t@gmail.com>
2021-05-19 20:14:00 +0700

    changing item name on MarketPresenter.kt

--- app/src/main/java/com/example/latber/presenter/MarketPresenter.kt
+++ app/src/main/java/com/example/latber/presenter/MarketPresenter.kt
@@ -15,12 +15,12 @@ class MarketPresenter(setView:MarketInterface) {
     //proses data
     fun ItemList(){
         //isi data
-        ItemsModel.add(Market_Item("https://img.cintamobil.com/2019/11/07/ZXn5Pos1/tertipu-beli-sparepart-mobil-4-9a07.jpg", "PS5", 1000000))
-        ItemsModel.add(Market_Item("https://jual-sparepart-motor.com/wp-content/uploads/2014/05/BEARING-LAHAR-1-260x280.jpg", "Pleys Tesien Lima", 1000000))
-        ItemsModel.add(Market_Item("https://momobil.id/news/wp-content/uploads/2018/12/mencari-toko-spare-part-mobil-terbaik-1024x576.jpg", "Laptop no. 1", 3000000))
-        ItemsModel.add(Market_Item("https://www.pandaindoteknik.co.id/wp-content/uploads/2019/06/Spare-Parts-Cover.jpg", "Mouse Gaming", 100000))
-        ItemsModel.add(Market_Item("https://5.imimg.com/data5/QD/DV/MY-17920646/perfect-gen-set-eicher-engine-250x250.jpg", "Keyboard", 150000))
-        ItemsModel.add(Market_Item("https://www.thecronutproject.com/wp-content/uploads/2021/01/Sparepart-motor.jpg", "Headset", 15000))
+        ItemsModel.add(Market_Item("https://img.cintamobil.com/2019/11/07/ZXn5Pos1/tertipu-beli-sparepart-mobil-4-9a07.jpg", "Aaaa", 1000000))
+        ItemsModel.add(Market_Item("https://jual-sparepart-motor.com/wp-content/uploads/2014/05/BEARING-LAHAR-1-260x280.jpg", "Bbbbb", 1000000))
+        ItemsModel.add(Market_Item("https://momobil.id/news/wp-content/uploads/2018/12/mencari-toko-spare-part-mobil-terbaik-1024x576.jpg", "Ccccc", 3000000))
+        ItemsModel.add(Market_Item("https://www.pandaindoteknik.co.id/wp-content/uploads/2019/06/Spare-Parts-Cover.jpg", "Dddddd", 100000))
+        ItemsModel.add(Market_Item("https://5.imimg.com/data5/QD/DV/MY-17920646/perfect-gen-set-eicher-engine-250x250.jpg", "Eeeeee", 150000))
+        ItemsModel.add(Market_Item("https://www.thecronutproject.com/wp-content/uploads/2021/01/Sparepart-motor.jpg", "Fffffff", 15000))
 
         //tampilkan data item ke dalam view dgn memanggil fungsi
         //ItemDetails yg ad pd MarketInterface dgn mengirimkan modelnya
